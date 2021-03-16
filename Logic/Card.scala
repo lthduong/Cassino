feb19fa866a6c61669bd603e183f6cd788042b5c
Duong Le <dng09010802v.2@gmail.com>
2021-03-17 00:08:07 +0700

    Interim report 17.03

--- Logic/Card.scala
+++ Logic/Card.scala
@@ -3,9 +3,9 @@ package Logic
 case class Card(name: String, suit: String, value: Int) {
 
   val handValue: Int = this match {
-    case Card("1", _, 1)           => 14
+    case Card("1" , _        , 1 ) => 14
     case Card("10", "Diamond", 10) => 16
-    case Card("2", "Spade", 2)     => 15
+    case Card("2" , "Spade"  , 2 ) => 15
     case _                         => value
   }
 
