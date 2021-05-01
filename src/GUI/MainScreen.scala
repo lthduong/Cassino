d9853489e6e08a59d80c230559d5ee0ad2b1642b
Duong Le <dng09010802v.2@gmail.com>
2021-05-02 01:59:51 +0700

    Update the GUI: the GUI now have the title, player and computer player choosing field

--- src/GUI/MainScreen.scala
+++ src/GUI/MainScreen.scala
@@ -5,42 +5,47 @@ import scala.swing.event._
 import java.awt.Color
 import src.Logic.Game
 
+import javax.swing.SwingConstants
+
 object MainScreen extends SimpleSwingApplication {
 
 
   // Creating the menu bar
   val save = new Button("Save")
   val load = new Button("Load")
-  val ins = new Button("Instruction")
+  val ins  = new Button("Instruction")
   val game = new Menu("Game") { contents ++= Vector(save, load) }
   val help = new Menu("Help") { contents += ins }
   val menu = new MenuBar { contents ++= Vector(game, help) }
 
 
   // Creating the computer player prompt and player selection
-  val cmpPrompt      = new Label("Do you want computer players?")
-  val computerPlayer = new Button("Yes")
-  val playerPrompt   = new Label("How many players?")
+  val cmpPrompt      = new Label("How many computer players?")
+  val cmpPlayers     = new ComboBox[Int](0 to 11)
+  val playerPrompt   = new Label("How many human players?")
   val playerSelect   = new ComboBox[Int](1 to 12)
-  val confirm        = new Button("Ok")
+  val playerCf      = new Button("Ok")
   val cmpPanel = new FlowPanel {
     contents  += cmpPrompt
-    contents  += computerPlayer
+    contents  += cmpPlayers
     //background = Color.green
   }
-
-
-  // The panel contains the player prompt
   val playerPanel = new FlowPanel {
     contents  += playerPrompt
     contents  += playerSelect
-    contents  += confirm
+    contents  += playerCf
     //background = Color.green
   }
 
 
   // Content of the screen
   val screenContent = new BoxPanel(Orientation.Vertical) {
+    contents += new Label("Cassino") {
+     preferredSize = new Dimension (150, 100)
+     font = new Font("Arial", 30, 30)
+     horizontalAlignment = Alignment.Center
+     verticalAlignment = Alignment.Center
+    }
     contents += cmpPanel
     contents += playerPanel
   }
@@ -54,8 +59,12 @@ object MainScreen extends SimpleSwingApplication {
     contents      = screenContent
   }
 
+
   // Event handling
   this.listenTo(ins)
+  this.listenTo(save)
+  this.listenTo(load)
+  this.listenTo(playerCf)
   this.reactions += {
     case clickEvent: ButtonClicked =>
       val source = clickEvent.source
