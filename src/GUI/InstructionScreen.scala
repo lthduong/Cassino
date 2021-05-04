8c642d54200cbf6386ba76907560e3ec44e9a2f4
Duong Le <dng09010802v.2@gmail.com>
2021-05-05 01:29:15 +0700

    Last save before changing the whole GUI structure

--- src/GUI/InstructionScreen.scala
+++ src/GUI/InstructionScreen.scala
@@ -4,16 +4,15 @@ import scala.swing._
 
 object InstructionScreen extends SimpleSwingApplication {
 
-  val instruction = new ScrollPane(rules)
+  val rule: String = "The deck is shuffled in the beginning of every round and the dealer deals 4 cards to every player (they are not visible to other players) and 4 cards on the table (visible for everyone). The rest of the cards are left on the table upside down. The player next to the dealer starts the game. On the next round he/she is the dealer.\nActions in each round:\nA player can play out one of his/her cards: it can be used either for taking cards from the table or to just putting it on the table. If the player cannot take anything from the table, he/she must put one of his/her cards on the table.\nIf the player takes cards from the table, he/she puts them in a separate pile of his/her own. The pile is used to count the points after the round has ended.\nThe number of cards on the table can vary. For example, if someone takes all the cards from the table, the next player must put a card on the empty table.\nPlayer must draw a new card from the deck after using a card so that he/she has always 4 cards in his/her hand. (When the deck runs out, everyone plays until there are no cards left in any player’s hand).\nPlayer can use a card to take one or more cards of the same value and cards such that their summed value is equal to the used card.\nSweep:\nIf some player gets all the cards from the table at the same time, he/she gets a so- called sweep which is written down.\nSpecial cards:\nThere are a couple of cards that are more valuable in the hand than in the table,\nAces: 14 in hand, 1 on table\nDiamonds-10: 16 in hand, 10 on table\nSpades-2: 15 in hand, 2 on table\nScoring: \nWhen every player runs out of cards, the last player to take cards from the table gets the rest of the cards from the table. After this the points are calculated and added to the existing scores.\nThe following things grant points:\nEach sweep grants 1 point.\nEach Ace grants 1 point.\nThe player with most cards gets 1 point.\nThe player with most spades gets 2 points.\nhe player with Diamonds-10 gets 2 points.\nThe player with Spades-2 gets 1 point."
+
+  val instruction = new ScrollPane {
+    contents = new Label(rule)
+  }
 
   def top = new Frame {
     title = "Instruction"
     contents = instruction
   }
-
-
-}
-
-object rules extends TextField {
 
 }
--- src/GUI/MainScreen.scala
+++ src/GUI/MainScreen.scala
@@ -1,11 +1,13 @@
 package src.GUI
 
+
+import scala.collection.mutable.Buffer
 import scala.swing._
 import scala.swing.event._
 import java.awt.Color
 import src.Logic.Game
-
 import javax.swing.SwingConstants
+
 
 object MainScreen extends SimpleSwingApplication {
 
@@ -19,35 +21,44 @@ object MainScreen extends SimpleSwingApplication {
   val menu = new MenuBar { contents ++= Vector(game, help) }
 
 
+
   // Creating the computer player prompt and player selection
   val cmpPrompt      = new Label("How many computer players?")
   val cmpPlayers     = new ComboBox[Int](0 to 11)
   val playerPrompt   = new Label("How many human players?")
-  val playerSelect   = new ComboBox[Int](1 to 12)
-  val playerCf      = new Button("Ok")
-  val cmpPanel = new FlowPanel {
-    contents  += cmpPrompt
-    contents  += cmpPlayers
-    //background = Color.green
+  val humPlayers     = new ComboBox[Int](1 to 12)
+  val playerCf       = new Button("Ok")
+
+  val plrPanel = new BoxPanel(Orientation.Vertical) {
+    background = new Color(4, 154, 49)
+    contents += new FlowPanel {
+      contents ++= Vector(cmpPrompt, cmpPlayers)
+      cmpPrompt.font = new Font("Arial", 0, 20)
+      background = new Color(4, 154, 49)
+      maximumSize = new Dimension(2000, 50)
+    }
+    contents += new FlowPanel {
+      contents ++= Vector(playerPrompt, humPlayers)
+      playerPrompt.font = new Font("Arial", 0, 20)
+      maximumSize = new Dimension(2000, 50)
+      background = new Color(4, 154, 49)
+    }
+    contents += playerCf
   }
-  val playerPanel = new FlowPanel {
-    contents  += playerPrompt
-    contents  += playerSelect
-    contents  += playerCf
-    //background = Color.green
-  }
+
 
 
   // Content of the screen
   val screenContent = new BoxPanel(Orientation.Vertical) {
     contents += new Label("Cassino") {
-     preferredSize = new Dimension (150, 100)
-     font = new Font("Arial", 30, 30)
+     horizontalTextPosition = Alignment.Center
+     verticalTextPosition = Alignment.Center
      horizontalAlignment = Alignment.Center
-     verticalAlignment = Alignment.Center
+     preferredSize = new Dimension (2000, 100)
+     font = new Font("Arial", 0, 75)
     }
-    contents += cmpPanel
-    contents += playerPanel
+    contents += plrPanel
+    background = new Color(4, 154, 49)
   }
 
 
@@ -55,7 +66,7 @@ object MainScreen extends SimpleSwingApplication {
   def top = new MainFrame {
     title         = "Cassino"
     menuBar       = menu
-    preferredSize = new Dimension(750, 500)
+    preferredSize = new Dimension(1500, 1000)
     contents      = screenContent
   }
 
@@ -70,6 +81,11 @@ object MainScreen extends SimpleSwingApplication {
       val source = clickEvent.source
       source match {
         case this.ins => InstructionScreen.top.visible = true
+        case this.playerCf => {
+          val nrPlayers = cmpPlayers.item + humPlayers.item
+          if(nrPlayers > 12) Dialog.showMessage(plrPanel, "The maximum number of players allowed is 12.", "Notice!")
+          else if(nrPlayers <= 1) Dialog.showMessage(plrPanel, "The game should have at least 2 players.", "Notice!")
+        }
       }
   }
 
