2a508e03cf1fc772c171559ca65704ef05d126e8
Duong Le <dng09010802v.2@gmail.com>
2021-05-05 03:38:01 +0700

    Continue refactoring the GUI structure. It now can take receive player's name

--- src/GUI/FirstScreen.scala
+++ src/GUI/FirstScreen.scala
@@ -14,44 +14,48 @@ object FirstScreen extends BoxPanel(Orientation.Vertical) {
   val title = new Label("Cassino")
   val plrCf = new Button("Ok")
   val plrPrompt = new Label("Number of human players")
-  val humPlayers = new ComboBox(1 to 12)
+  val humPlayers = new ComboBox(1 to 6)
   val cmpPrompt = new Label("Number of computer players")
-  val cmpPlayers = new ComboBox(0 to 11)
+  val cmpPlayers = new ComboBox(0 to 3)
   val nameCf = new Button("Confirm")
+
 
   val titlePanel = new FlowPanel {
     title.font = new Font("Arial", 0, 60)
-    title.background = new Color(0, 153, 0)
-    background = new Color(0, 153, 0)
+    maximumSize = new Dimension(1200, 100)
     contents += title
   }
+
 
   // The place where the user can type the number of player they want
   val plrPanel = new BoxPanel(Orientation.Vertical) {
     contents += new FlowPanel {
       contents ++= Vector(cmpPrompt, cmpPlayers)
-      maximumSize = new Dimension(1400, 200)
+      maximumSize = new Dimension(1400, 100)
       background = new Color(0, 153, 0)
     }
     contents += new FlowPanel {
       contents ++= Vector(plrPrompt, humPlayers)
-      preferredSize = new Dimension(1400, 200)
+      maximumSize = new Dimension(1400, 100)
       background = new Color(0, 153, 0)
     }
     contents += plrCf
     background = new Color(0, 153, 0)
   }
 
+
   // This method will create a panel to input names
   def getName(number: Int): BoxPanel = {
     val namePrompt = new BoxPanel(Orientation.Vertical) {
       for( i <- 1 to number ) {
-        contents += new FlowPanel {
+        val namePanel = new FlowPanel {
           contents += new Label("Input player's name:")
-          contents += new TextField(50)
+          contents += new TextField(10)
+          background = new Color(0, 153, 0)
         }
+        contents += namePanel
       }
-      foreground = new Color(0, 153, 0)
+      background = new Color(0, 153, 0)
       contents += nameCf
     }
     namePrompt
@@ -59,6 +63,7 @@ object FirstScreen extends BoxPanel(Orientation.Vertical) {
 
   contents += title
   contents += plrPanel
+  background = new Color(0, 153, 0)
 
   this.listenTo(plrCf)
   this.listenTo(nameCf)
@@ -68,11 +73,10 @@ object FirstScreen extends BoxPanel(Orientation.Vertical) {
       button match {
         case this.plrCf => {
           val nrPlayers = cmpPlayers.item + humPlayers.item
-          if(nrPlayers > 12) Dialog.showMessage(plrPanel, "The maximum number of players allowed is 12.", "Notice!")
-          else if(nrPlayers <= 1) Dialog.showMessage(plrPanel, "The game should have at least 2 players.", "Notice!")
-          else contents += getName(nrPlayers)
+          val nameInput = getName(humPlayers.item)
+          this.plrPanel.contents.clear()
+          contents += getName(humPlayers.item)
           this.revalidate()
-          this.repaint
         }
       }
   }
